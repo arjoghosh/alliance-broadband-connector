38335c18d42563445ef4bdb24d94c497350f140e
Arjo Ghosh আর্য্য ঘোষ <arjoghosh@outlook.com>
2016-05-23 22:33:52 +0530

    Create alliance-broadband-connector.py

---
+++ alliance-broadband-connector.py
@@ -0,0 +1,10 @@
+import requests
+x=input("Enter username : ")
+x=x.strip()
+y=input("Enter password : ")
+params = {'user': x, 'pass': y, 'login':'Login'}
+r = requests.post("http://10.254.254.24/0/up/", data=params)
+if r.status_code==200:
+    print('Your Internet connection is configured properly.')
+else:
+   print('Something went wrong. Please try again later.')
